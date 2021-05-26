ddf3a4b5e8a0d80dc3c9fb0d4a0e440567ec7673
hm-qm <77652802+hm-qm@users.noreply.github.com>
2021-05-26 21:12:38 +1000

    Initial Commit with base files

---
+++ README.md
@@ -0,0 +1,16 @@
+# Ez To-Do List
+
+A simple and functional To-Do list app created using HTML5, CSS3 and JavaScript.
+
+Base features added:
+
+- Simple and clean UI
+- Item check and delete buttons
+- 'To Do' item is added to a list using DOM manipulation
+
+Features to be added:
+
+- Add check and delete functions to the respective buttons
+- Clear all items
+- re-order items
+- edit items
---
+++ app.js
@@ -0,0 +1,42 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+
+// Functions
+function addTodo(event){
+    // Prevents form from submitting
+    event.preventDefault();
+    // Create Todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    // Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    // Check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    // Trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    // Append to list
+    todoList.appendChild(todoDiv);
+    // Clear todoInput value
+    todoInput.value = "";
+}
+
+// function deleteCheck(e){
+//     const item = e.target;
+//     // Delete todo
+//     if(item.classList[0] === '')
+// }
